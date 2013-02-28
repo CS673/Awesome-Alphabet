0405b963cd1d90ae14bf6cd2dbb015da3f6c7e3c
Levi <levippaul@gmail.com>
2013-02-28 02:29:59 -0500

    created new folder, test, to separate test code from production code

---
+++ Awesome-Alphabet/test/edu/bu/cs673/AwesomeAlphabet/controller/AlphabetPageControllerTest.java
@@ -0,0 +1,211 @@
+package edu.bu.cs673.AwesomeAlphabet.controller;
+
+import java.util.Iterator;
+
+import edu.bu.cs673.AwesomeAlphabet.controller.AlphabetPageController;
+import edu.bu.cs673.AwesomeAlphabet.model.IPageObserver;
+import edu.bu.cs673.AwesomeAlphabet.view.AlphabetPageView;
+import edu.bu.cs673.AwesomeAlphabet.model.MainWindow;
+import edu.bu.cs673.AwesomeAlphabet.model.Letter;
+import org.junit.*;
+import edu.bu.cs673.AwesomeAlphabet.model.Alphabet;
+import static org.junit.Assert.*;
+
+/**
+ * The class <code>AlphabetPageControllerTest</code> contains tests for the class <code>{@link AlphabetPageController}</code>.
+ *
+ * @generatedBy CodePro at 2/22/13 1:54 AM
+ * @author Levi
+ * @version $Revision: 1.0 $
+ */
+public class AlphabetPageControllerTest {
+	/**
+	 * Run the AlphabetPageController(IPageObserver,AlphabetPageView,Alphabet) constructor test.
+	 *
+	 * @throws Exception
+	 *
+	 * @generatedBy CodePro at 2/22/13 1:54 AM
+	 */
+	@Test
+	public void testAlphabetPageController_1()
+		throws Exception {
+		IPageObserver pageObserver = new MainWindow();
+		AlphabetPageView view = new AlphabetPageView("");
+		Alphabet alphabet = new Alphabet();
+
+		AlphabetPageController result = new AlphabetPageController(pageObserver, view, alphabet);
+
+		// add additional test code here
+		assertNotNull(result);
+		assertEquals(true, result.GoToTitlePage());
+	}
+
+	/**
+	 * Run the Iterator<Letter> GetLetterIterator() method test.
+	 *
+	 * @throws Exception
+	 *
+	 * @generatedBy CodePro at 2/22/13 1:54 AM
+	 */
+	@Test
+	public void testGetLetterIterator_1()
+		throws Exception {
+		AlphabetPageController fixture = new AlphabetPageController(new MainWindow(), new AlphabetPageView(""), new Alphabet());
+
+		Iterator<Letter> result = fixture.GetLetterIterator();
+
+		// add additional test code here
+		assertNotNull(result);
+		assertEquals(true, result.hasNext());
+	}
+
+	/**
+	 * Run the boolean GoToLetterPage(Letter) method test.
+	 *
+	 * @throws Exception
+	 *
+	 * @generatedBy CodePro at 2/22/13 1:54 AM
+	 */
+	@Test
+	public void testGoToLetterPage_1()
+		throws Exception {
+		AlphabetPageController fixture = new AlphabetPageController(new MainWindow(), new AlphabetPageView(""), new Alphabet());
+		Letter cLetter = new Letter('');
+
+		boolean result = fixture.GoToLetterPage(cLetter);
+
+		// add additional test code here
+		assertEquals(false, result);
+	}
+
+	/**
+	 * Run the boolean GoToLetterPage(Letter) method test.
+	 *
+	 * @throws Exception
+	 *
+	 * @generatedBy CodePro at 2/22/13 1:54 AM
+	 */
+	@Test
+	public void testGoToLetterPage_2()
+		throws Exception {
+		AlphabetPageController fixture = new AlphabetPageController(new MainWindow(), new AlphabetPageView(""), new Alphabet());
+		Letter cLetter = new Letter('');
+
+		boolean result = fixture.GoToLetterPage(cLetter);
+
+		// add additional test code here
+		assertEquals(false, result);
+	}
+
+	/**
+	 * Run the boolean GoToLetterPage(Letter) method test.
+	 *
+	 * @throws Exception
+	 *
+	 * @generatedBy CodePro at 2/22/13 1:54 AM
+	 */
+	@Test
+	public void testGoToLetterPage_3()
+		throws Exception {
+		AlphabetPageController fixture = new AlphabetPageController(new MainWindow(), new AlphabetPageView(""), new Alphabet());
+		Letter cLetter = new Letter('');
+
+		boolean result = fixture.GoToLetterPage(cLetter);
+
+		// add additional test code here
+		assertEquals(false, result);
+	}
+
+	/**
+	 * Run the boolean GoToTitlePage() method test.
+	 *
+	 * @throws Exception
+	 *
+	 * @generatedBy CodePro at 2/22/13 1:54 AM
+	 */
+	@Test
+	public void testGoToTitlePage_1()
+		throws Exception {
+		AlphabetPageController fixture = new AlphabetPageController(new MainWindow(), new AlphabetPageView(""), new Alphabet());
+
+		boolean result = fixture.GoToTitlePage();
+
+		// add additional test code here
+		assertEquals(true, result);
+	}
+
+	/**
+	 * Run the boolean GoToTitlePage() method test.
+	 *
+	 * @throws Exception
+	 *
+	 * @generatedBy CodePro at 2/22/13 1:54 AM
+	 */
+	@Test
+	public void testGoToTitlePage_2()
+		throws Exception {
+		AlphabetPageController fixture = new AlphabetPageController(new MainWindow(), new AlphabetPageView(""), new Alphabet());
+
+		boolean result = fixture.GoToTitlePage();
+
+		// add additional test code here
+		assertEquals(true, result);
+	}
+
+	/**
+	 * Run the boolean PlayAlphabetSong() method test.
+	 *
+	 * @throws Exception
+	 *
+	 * @generatedBy CodePro at 2/22/13 1:54 AM
+	 */
+	@Test(expected = sun.reflect.generics.reflectiveObjects.NotImplementedException.class)
+	public void testPlayAlphabetSong_1()
+		throws Exception {
+		AlphabetPageController fixture = new AlphabetPageController(new MainWindow(), new AlphabetPageView(""), new Alphabet());
+
+		boolean result = fixture.PlayAlphabetSong();
+
+		// add additional test code here
+		assertTrue(result);
+	}
+
+	/**
+	 * Perform pre-test initialization.
+	 *
+	 * @throws Exception
+	 *         if the initialization fails for some reason
+	 *
+	 * @generatedBy CodePro at 2/22/13 1:54 AM
+	 */
+	@Before
+	public void setUp()
+		throws Exception {
+		// add additional set up code here
+	}
+
+	/**
+	 * Perform post-test clean-up.
+	 *
+	 * @throws Exception
+	 *         if the clean-up fails for some reason
+	 *
+	 * @generatedBy CodePro at 2/22/13 1:54 AM
+	 */
+	@After
+	public void tearDown()
+		throws Exception {
+		// Add additional tear down code here
+	}
+
+	/**
+	 * Launch the test.
+	 *
+	 * @param args the command line arguments
+	 *
+	 * @generatedBy CodePro at 2/22/13 1:54 AM
+	 */
+	public static void main(String[] args) {
+		new org.junit.runner.JUnitCore().run(AlphabetPageControllerTest.class);
+	}
+}
---
+++ Awesome-Alphabet/test/edu/bu/cs673/AwesomeAlphabet/model/AlphabetTest.java
@@ -0,0 +1,427 @@
+package edu.bu.cs673.AwesomeAlphabet.model;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.Iterator;
+import java.util.Properties;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import edu.bu.cs673.AwesomeAlphabet.model.Alphabet;
+import edu.bu.cs673.AwesomeAlphabet.model.Letter;
+import edu.bu.cs673.AwesomeAlphabet.view.AlphabetPageView;
+
+/**
+ * The class <code>AlphabetTest</code> contains tests for the class <code>{@link Alphabet}</code>.
+ *
+ * @generatedBy CodePro at 2/22/13 1:46 AM
+ * @author Levi
+ * @version $Revision: 1.0 $
+ */
+public class AlphabetTest {
+	/**
+	 * Run the Alphabet() constructor test.
+	 *
+	 * @throws Exception
+	 *
+	 * @generatedBy CodePro at 2/22/13 1:46 AM
+	 */
+	@Test
+	public void testAlphabet_1()
+		throws Exception {
+
+		Alphabet result = new Alphabet();
+
+		// add additional test code here
+		assertNotNull(result);
+		assertEquals(null, result.GoToPreviousLetter());
+		assertEquals(false, result.hasChanged());
+		assertEquals(0, result.countObservers());
+	}
+
+	/**
+	 * Run the Alphabet() constructor test.
+	 *
+	 * @throws Exception
+	 *
+	 * @generatedBy CodePro at 2/22/13 1:46 AM
+	 */
+	@Test
+	public void testAlphabet_2()
+		throws Exception {
+
+		Alphabet result = new Alphabet();
+
+		// add additional test code here
+		assertNotNull(result);
+		assertEquals(null, result.GoToPreviousLetter());
+		assertEquals(false, result.hasChanged());
+		assertEquals(0, result.countObservers());
+	}
+
+	/**
+	 * Run the Letter GetCurrentLetter() method test.
+	 *
+	 * @throws Exception
+	 *
+	 * @generatedBy CodePro at 2/22/13 1:46 AM
+	 */
+	@Test
+	public void testGetCurrentLetter_1()
+		throws Exception {
+		Alphabet fixture = new Alphabet();
+		fixture.m_iCurLetterIndex = 1;
+		fixture.addObserver(new AlphabetPageView(""));
+
+		Letter result = fixture.GetCurrentLetter();
+
+		// add additional test code here
+		assertNotNull(result);
+		assertEquals(null, result.getWord());
+		assertEquals('B', result.GetUppercaseLetter());
+		assertEquals('b', result.GetLetterAsChar());
+		assertEquals(false, result.hasChanged());
+		assertEquals(0, result.countObservers());
+	}
+
+	/**
+	 * Run the Iterator<Letter> GetIterator() method test.
+	 *
+	 * @throws Exception
+	 *
+	 * @generatedBy CodePro at 2/22/13 1:46 AM
+	 */
+	@Test
+	public void testGetIterator_1()
+		throws Exception {
+		Alphabet fixture = new Alphabet();
+		fixture.m_iCurLetterIndex = 1;
+		fixture.addObserver(new AlphabetPageView(""));
+
+		Iterator<Letter> result = fixture.GetIterator();
+
+		// add additional test code here
+		assertNotNull(result);
+		assertEquals(true, result.hasNext());
+	}
+
+	/**
+	 * Run the Letter GoToNextLetter() method test.
+	 *
+	 * @throws Exception
+	 *
+	 * @generatedBy CodePro at 2/22/13 1:46 AM
+	 */
+	@Test
+	public void testGoToNextLetter_1()
+		throws Exception {
+		Alphabet fixture = new Alphabet();
+		fixture.m_iCurLetterIndex = 1;
+		fixture.addObserver(new AlphabetPageView(""));
+
+		Letter result = fixture.GoToNextLetter();
+
+		// add additional test code here
+		assertNotNull(result);
+		assertEquals(null, result.getWord());
+		assertEquals('C', result.GetUppercaseLetter());
+		assertEquals('c', result.GetLetterAsChar());
+		assertEquals(false, result.hasChanged());
+		assertEquals(0, result.countObservers());
+	}
+
+	/**
+	 * Run the Letter GoToNextLetter() method test.
+	 *
+	 * @throws Exception
+	 *
+	 * @generatedBy CodePro at 2/22/13 1:46 AM
+	 */
+	@Test
+	public void testGoToNextLetter_2()
+		throws Exception {
+		Alphabet fixture = new Alphabet();
+		fixture.m_iCurLetterIndex = 1;
+		fixture.addObserver(new AlphabetPageView(""));
+
+		Letter result = fixture.GoToNextLetter();
+
+		// add additional test code here
+		assertNotNull(result);
+		assertEquals(null, result.getWord());
+		assertEquals('C', result.GetUppercaseLetter());
+		assertEquals('c', result.GetLetterAsChar());
+		assertEquals(false, result.hasChanged());
+		assertEquals(0, result.countObservers());
+	}
+
+	/**
+	 * Run the Letter GoToPreviousLetter() method test.
+	 *
+	 * @throws Exception
+	 *
+	 * @generatedBy CodePro at 2/22/13 1:46 AM
+	 */
+	@Test
+	public void testGoToPreviousLetter_1()
+		throws Exception {
+		Alphabet fixture = new Alphabet();
+		fixture.m_iCurLetterIndex = 0;
+		fixture.addObserver(new AlphabetPageView(""));
+
+		Letter result = fixture.GoToPreviousLetter();
+
+		// add additional test code here
+		assertEquals(null, result);
+	}
+
+	/**
+	 * Run the Letter GoToPreviousLetter() method test.
+	 *
+	 * @throws Exception
+	 *
+	 * @generatedBy CodePro at 2/22/13 1:46 AM
+	 */
+	@Test
+	public void testGoToPreviousLetter_2()
+		throws Exception {
+		Alphabet fixture = new Alphabet();
+		fixture.m_iCurLetterIndex = 1;
+		fixture.addObserver(new AlphabetPageView(""));
+
+		Letter result = fixture.GoToPreviousLetter();
+
+		// add additional test code here
+		assertNotNull(result);
+		assertEquals(null, result.getWord());
+		assertEquals('A', result.GetUppercaseLetter());
+		assertEquals('a', result.GetLetterAsChar());
+		assertEquals(false, result.hasChanged());
+		assertEquals(0, result.countObservers());
+	}
+
+	/**
+	 * Run the void LoadResources(Properties) method test.
+	 *
+	 * @throws Exception
+	 *
+	 * @generatedBy CodePro at 2/22/13 1:46 AM
+	 */
+	@Test
+	public void testLoadResources_1()
+		throws Exception {
+		Alphabet fixture = new Alphabet();
+		fixture.m_iCurLetterIndex = 1;
+		fixture.addObserver(new AlphabetPageView(""));
+		Properties prop = new Properties();
+
+		fixture.LoadResources(prop);
+
+		// add additional test code here
+	}
+
+	/**
+	 * Run the void LoadResources(Properties) method test.
+	 *
+	 * @throws Exception
+	 *
+	 * @generatedBy CodePro at 2/22/13 1:46 AM
+	 */
+	@Test
+	public void testLoadResources_2()
+		throws Exception {
+		Alphabet fixture = new Alphabet();
+		fixture.m_iCurLetterIndex = 1;
+		fixture.addObserver(new AlphabetPageView(""));
+		Properties prop = new Properties();
+
+		fixture.LoadResources(prop);
+
+		// add additional test code here
+	}
+
+	/**
+	 * Run the void LoadResources(Properties) method test.
+	 *
+	 * @throws Exception
+	 *
+	 * @generatedBy CodePro at 2/22/13 1:46 AM
+	 */
+	@Test
+	public void testLoadResources_3()
+		throws Exception {
+		Alphabet fixture = new Alphabet();
+		fixture.m_iCurLetterIndex = 1;
+		fixture.addObserver(new AlphabetPageView(""));
+		Properties prop = new Properties();
+
+		fixture.LoadResources(prop);
+
+		// add additional test code here
+	}
+
+	/**
+	 * Run the void LoadResources(Properties) method test.
+	 *
+	 * @throws Exception
+	 *
+	 * @generatedBy CodePro at 2/22/13 1:46 AM
+	 */
+	@Test
+	public void testLoadResources_4()
+		throws Exception {
+		Alphabet fixture = new Alphabet();
+		fixture.m_iCurLetterIndex = 1;
+		fixture.addObserver(new AlphabetPageView(""));
+		Properties prop = new Properties();
+
+		fixture.LoadResources(prop);
+
+		// add additional test code here
+	}
+
+	/**
+	 * Run the void LoadResources(Properties) method test.
+	 *
+	 * @throws Exception
+	 *
+	 * @generatedBy CodePro at 2/22/13 1:46 AM
+	 */
+	@Test
+	public void testLoadResources_5()
+		throws Exception {
+		Alphabet fixture = new Alphabet();
+		fixture.m_iCurLetterIndex = 1;
+		fixture.addObserver(new AlphabetPageView(""));
+		Properties prop = new Properties();
+
+		fixture.LoadResources(prop);
+
+		// add additional test code here
+	}
+
+	/**
+	 * Run the void LoadResources(Properties) method test.
+	 *
+	 * @throws Exception
+	 *
+	 * @generatedBy CodePro at 2/22/13 1:46 AM
+	 */
+	@Test
+	public void testLoadResources_6()
+		throws Exception {
+		Alphabet fixture = new Alphabet();
+		fixture.m_iCurLetterIndex = 1;
+		fixture.addObserver(new AlphabetPageView(""));
+		Properties prop = new Properties();
+
+		fixture.LoadResources(prop);
+
+		// add additional test code here
+	}
+
+	/**
+	 * Run the Letter SetCurrentLetter(Letter) method test.
+	 *
+	 * @throws Exception
+	 *
+	 * @generatedBy CodePro at 2/22/13 1:46 AM
+	 */
+	@Test
+	public void testSetCurrentLetter_1()
+		throws Exception {
+		Alphabet fixture = new Alphabet();
+		fixture.m_iCurLetterIndex = 1;
+		fixture.addObserver(new AlphabetPageView(""));
+		Letter cLetter = new Letter('');
+
+		Letter result = fixture.SetCurrentLetter(cLetter);
+
+		// add additional test code here
+		assertEquals(null, result);
+	}
+
+	/**
+	 * Run the Letter SetCurrentLetter(Letter) method test.
+	 *
+	 * @throws Exception
+	 *
+	 * @generatedBy CodePro at 2/22/13 1:46 AM
+	 */
+	@Test
+	public void testSetCurrentLetter_2()
+		throws Exception {
+		Alphabet fixture = new Alphabet();
+		fixture.m_iCurLetterIndex = 1;
+		fixture.addObserver(new AlphabetPageView(""));
+		Letter cLetter = new Letter('');
+
+		Letter result = fixture.SetCurrentLetter(cLetter);
+
+		// add additional test code here
+		assertEquals(null, result);
+	}
+
+	/**
+	 * Run the Letter SetCurrentLetter(Letter) method test.
+	 *
+	 * @throws Exception
+	 *
+	 * @generatedBy CodePro at 2/22/13 1:46 AM
+	 */
+	@Test
+	public void testSetCurrentLetter_3()
+		throws Exception {
+		Alphabet fixture = new Alphabet();
+		fixture.m_iCurLetterIndex = 1;
+		fixture.addObserver(new AlphabetPageView(""));
+		Letter cLetter = new Letter('');
+
+		Letter result = fixture.SetCurrentLetter(cLetter);
+
+		// add additional test code here
+		assertEquals(null, result);
+	}
+
+	/**
+	 * Perform pre-test initialization.
+	 *
+	 * @throws Exception
+	 *         if the initialization fails for some reason
+	 *
+	 * @generatedBy CodePro at 2/22/13 1:46 AM
+	 */
+	@Before
+	public void setUp()
+		throws Exception {
+		// add additional set up code here
+	}
+
+	/**
+	 * Perform post-test clean-up.
+	 *
+	 * @throws Exception
+	 *         if the clean-up fails for some reason
+	 *
+	 * @generatedBy CodePro at 2/22/13 1:46 AM
+	 */
+	@After
+	public void tearDown()
+		throws Exception {
+		// Add additional tear down code here
+	}
+
+	/**
+	 * Launch the test.
+	 *
+	 * @param args the command line arguments
+	 *
+	 * @generatedBy CodePro at 2/22/13 1:46 AM
+	 */
+	public static void main(String[] args) {
+		new org.junit.runner.JUnitCore().run(AlphabetTest.class);
+	}
+}
